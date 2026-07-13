baba27111b9e67b2e3caece189f28d2a58cd1c3c
Ananda Rizky Yuliansyah <ananda.yuliansyah@ai.astra.co.id>
2026-07-13 23:00:18 +0700

    feat: downgrade android agp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.paymentobserver"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.paymentobserver"
         minSdk = 21
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
