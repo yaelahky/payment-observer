4ea6367ac4b2ac1af769a3b050490cfb2ed7d6ac
Ananda Rizky Yuliansyah <ananda.yuliansyah@ai.astra.co.id>
2026-07-15 15:36:57 +0700

    chore: bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.fgteam.paymentobserver"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
